262b1890835eaa0ae4f015e58dca3a5f8f0e5d7f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-08 15:26:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function saturdayFun(activity="roller-skate") {
 }
 
 saturdayFun();
+
+
+const mondayWork = function() {
+  console.log("This Monday, I will ${event}.");
+}
+mondayWork(go shopping);
+
+// This Monday, I will ... ."
+// activity passed in as the first parameter. IF nothing is passed in, default to "go to the office".
