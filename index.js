a973906a494b836a890c42b8f4e3450bef397d2d
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-08 16:36:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 function saturdayFun(activity= "roller-skate"){
-  return `This Saturday, I will ${activity}!`
+  return `This Saturday, I want to ${activity}!`
 }
 
 const mondayWork = function(activity="go to the office") {
