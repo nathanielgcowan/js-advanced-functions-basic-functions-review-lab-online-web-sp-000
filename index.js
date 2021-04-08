5d8371a4c71ad1d5656d20e985b4de6a50934b46
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-08 16:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function saturdayFun(activity= "roller-skate"){
 }
 
 const mondayWork = function(activity="go to the office") {
-  return `"This Monday, I will ${activity}.`
+  return `This Monday, I will ${activity}.`
 }
 mondayWork();
 
